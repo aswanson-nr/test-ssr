9ad76b9ef438f7a1ba64e78d4574e2b31cdb0cb9
aswanson-nr <aswanson@newrelic.com>
2021-12-08 16:40:27 -0800

    add ssr page

---
+++ src/pages/instant-observability.js
@@ -0,0 +1,127 @@
+import PropTypes from "prop-types"
+import React from "react"
+const QUICKSTARTS_QUERY = `
+{
+  actor {
+    nr1Catalog {
+      quickstarts {
+        totalCount
+        results {
+          id
+          sourceUrl
+          supportLevel
+          metadata {
+            slug
+            summary
+            authors {
+              name
+            }
+            description
+            displayName
+            installer {
+              type
+              ... on Nr1CatalogInstallPlan {
+                steps {
+                  id
+                  displayName
+                }
+              }
+            }
+            icon {
+              url
+            }
+            keywords
+            quickstartComponents {
+              ... on Nr1CatalogQuickstartDocumentation {
+                __typename
+                metadata {
+                  displayName
+                  url
+                  description
+                }
+              }
+              ... on Nr1CatalogQuickstartDashboard {
+                __typename
+                metadata {
+                  description
+                  displayName
+                  previews {
+                    url
+                  }
+                }
+              }
+              ... on Nr1CatalogQuickstartAlertCondition {
+                __typename
+                metadata {
+                  description
+                  displayName
+                  type
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`
+
+const NERDGRAPH_URL = process.env.NERDGRAPH_URL
+const NEW_RELIC_API_KEY = process.env.NEW_RELIC_API_KEY
+export const getServerData = async () => {
+  try {
+    console.log("url", NERDGRAPH_URL)
+    const resp = await fetch(NERDGRAPH_URL, {
+      method: "POST",
+      body: JSON.stringify({ query: QUICKSTARTS_QUERY }),
+      headers: {
+        "Content-Type": "application/json",
+        "Api-Key": NEW_RELIC_API_KEY,
+      },
+    })
+
+    if (!resp.ok) {
+      throw Error(`Non 200 status code returned`, resp.status, resp.statusText)
+    }
+
+    const json = await resp.json()
+
+    if (json.data?.errors) {
+      throw Error(`Errors returned from nerdgraph`, json.data.errors)
+    }
+
+    const quickstarts = json.data?.actor?.nr1Catalog?.quickstarts
+    console.log(`Found ${quickstarts.totalCount} quickstarts`)
+
+    return {
+      props: {
+        error: false,
+        data: json.data,
+      },
+    }
+  } catch (err) {
+    /* eslint-disable-next-line no-console */
+    console.error(err)
+
+    return {
+      props: {
+        error: true,
+      },
+    }
+  }
+}
+
+const QuickstartsPageSSR = ({ serverData, location }) => {
+  const quickstarts =
+    serverData.data?.actor?.nr1Catalog?.quickstarts?.results || []
+
+  return <p>{JSON.stringify(serverData)}</p>
+}
+
+QuickstartsPageSSR.propTypes = {
+  serverData: PropTypes.object.isRequired,
+  location: PropTypes.object.isRequired,
+}
+
+export default QuickstartsPageSSR
